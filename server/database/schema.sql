17e1e2b39c91717de117ab2e7ddb989c5f5b1990
ViriatoF <viriato.ferreira44@gmail.com>
2025-02-12 12:17:34 +0100

    🔨 Add ressources folder with .excalidraw and schema.sql with new data

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -88,6 +88,7 @@ CREATE TABLE `user` (
   `email` varchar(45) NOT NULL,
   `matricule` int NOT NULL,
   `role` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -110,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-12 11:59:50
+-- Dump completed on 2025-02-12 12:16:10
